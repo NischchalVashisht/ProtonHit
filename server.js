7556e4243d243ba29192def30d394e6f599d7e2c
Nishchal vashisht <47364503+NischchalVashisht@users.noreply.github.com>
2023-03-31 10:09:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,9 +30,7 @@ console.log(pm);
   pm.close()
 
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/build/index.html'));
-});
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
